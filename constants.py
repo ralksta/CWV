566eefbc97835015d458e75e70889938dd032ff6
ralksta <ralksta@posteo.de>
2021-12-06 11:20:50 +0100

    Add your API KEY here

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""
+constants.py: Add your CRUX API Key here
+
+Get a key at:
+https://developers.google.com/web/tools/chrome-user-experience-report/api/guides/getting-started
+
+"""
+
+crux_api_key = 'YOUR API KEY'
